3e51a1afee87b45e089f5dbf236d123d170c725f
yupolischuk <yumuscletank@gmail.com>
2018-08-03 20:37:04 +0300

    Test commit

--- flashlearn/db_default.py
+++ flashlearn/db_default.py
@@ -1,10 +1,9 @@
 import sqlite3
 
-
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
-# from flask_sqlalchemy import SQLAlchemy
+
 
 def get_db():
     if 'db' not in g:
--- setup.py
+++ setup.py
@@ -9,4 +9,4 @@ setup(
     install_requires=[
         'flask',
     ],
-)
+) 
